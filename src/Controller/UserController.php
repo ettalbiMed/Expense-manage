26bf07c583e2e8e046ee9ebd11bddbc0f808f25f
ettalbiMed <58626889+ettalbiMed@users.noreply.github.com>
2020-10-05 19:01:19 +0100

    Add files via upload

---
+++ src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/user")
+ */
+class UserController extends AbstractController {
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function profil(Request $request) {
+        $user = $this->getUser();
+        dump($user);
+        die();
+        return $this->render('User/profil.html.twig');
+    }
+
+}
---
+++ src/DoctrineListener/ApplicationCreationListener.php
@@ -0,0 +1,33 @@
+<?php
+// src/App/DoctrineListener/ApplicationCreationListener.php
+
+namespace App\DoctrineListener;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use App\Email\ApplicationMailer;
+use App\Entity\Application;
+
+class ApplicationCreationListener
+{
+  /**
+   * @var ApplicationMailer
+   */
+  private $applicationMailer;
+
+  public function __construct(ApplicationMailer $applicationMailer)
+  {
+    $this->applicationMailer = $applicationMailer;
+  }
+
+  public function postPersist(LifecycleEventArgs $args)
+  {
+    $entity = $args->getObject();
+
+    // On ne veut envoyer un email que pour les entités Application
+    if (!$entity instanceof Application) {
+      return;
+    }
+
+    $this->applicationMailer->sendNewNotification($entity);
+  }
+}
